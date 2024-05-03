c8f6a35bb47ce524be3791ae1773040e96251bd8
서혜리 <hye_dam_2@naver.com>
2024-05-03 16:27:59 +0900

    Ranks entity 작성 완

---
+++ src/main/java/com/allrounders/goalkeeper/domain/Ranks.java
@@ -0,0 +1,25 @@
+package com.allrounders.goalkeeper.domain;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Ranks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer rankId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")   // foreign key (member_id) references Member (member_id)
+    private Member member;
+
+    @ColumnDefault("0")
+    private Integer rankPoint;
+}
