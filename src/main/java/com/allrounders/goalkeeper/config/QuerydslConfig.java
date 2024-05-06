203b3a825aeb4f664e0a46bbec15aa769eab35dd
서혜리 <hye_dam_2@naver.com>
2024-05-06 18:04:29 +0900

    QuerydslConfig 수정

--- src/main/java/com/allrounders/goalkeeper/config/QuerydslConfig.java
+++ src/main/java/com/allrounders/goalkeeper/config/QuerydslConfig.java
@@ -3,12 +3,10 @@ package com.allrounders.goalkeeper.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@EnableJpaAuditing
 @Configuration
 public class QuerydslConfig {
 
