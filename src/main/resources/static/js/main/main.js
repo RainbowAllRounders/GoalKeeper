e44264234fcf4ce96b1b200b3bcaa7936bbf8d0d
Gaannini <0107162a@gmail.com>
2024-05-06 17:57:12 +0900

    fix : goalmainJsFixing
    좋아요js코드 수정
    + footer notion링크수정

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -1,6 +1,5 @@
 const rankPage = document.getElementById("rankpage");
 const header = document.getElementById("header");
-const hotgoal = document.getElementById("hotgoal");
 const rankArrow = document.getElementById("rankarrow");
 
 let isScrollingDown = false;
@@ -20,10 +19,26 @@ const observerUp = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting && isScrollingDown) {
             isScrollingDown = false;  // 스크롤 방향 전환
-            window.scroll({top: hotgoal.offsetTop-50, behavior: 'smooth'});
+            window.scroll({top: 0, behavior: 'smooth'});
         }
     });
 }, {threshold: 0.3});// threshold를 조정하여 얼마나 많은 부분이 보여졌을 때 이벤트를 발생시킬지 결정
 
 observerDown.observe(rankPage);
 observerUp.observe(rankArrow);
+
+// 좋아요
+const likeIcons = document.querySelectorAll('.like_F, .like_T');
+
+likeIcons.forEach(icon => {
+    icon.addEventListener('click', function() {
+        // 클래스 토글을 사용하여 하트 이미지 변경
+        if (icon.classList.contains('like_F')) {
+            icon.classList.replace('like_F', 'like_T');
+            // icon.style.backgroundImage = 'url(../../images/main/redHeart.png)';
+        } else {
+            icon.classList.replace('like_T', 'like_F');
+            // icon.style.backgroundImage = 'url(../../images/main/Heart.png)';
+        }
+    });
+});
