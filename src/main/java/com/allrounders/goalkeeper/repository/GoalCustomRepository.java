f498f6d9b9e85209861c279ec01841029a90180d
서혜리 <hye_dam_2@naver.com>
2024-05-07 14:57:47 +0900

    Querydsl용 GoalCustomRepository 생성

---
+++ src/main/java/com/allrounders/goalkeeper/repository/GoalCustomRepository.java
@@ -0,0 +1,9 @@
+package com.allrounders.goalkeeper.repository;
+
+import com.allrounders.goalkeeper.domain.Goal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface GoalCustomRepository {
+    Page<Goal> findAllOrderByCreatedDateDesc(Pageable pageable);
+}
