72028cb352e2f2ae8018f782ac7053ed0a479eec
서혜리 <hye_dam_2@naver.com>
2024-05-08 10:24:10 +0900

    GoalRepositoryTests 작성 중 - goalAdd 기능 테스트

---
+++ src/test/java/com/allrounders/goalkeeper/repository/GoalRepositoryTests.java
@@ -0,0 +1,46 @@
+package com.allrounders.goalkeeper.repository;
+
+import com.allrounders.goalkeeper.domain.Goal;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class GoalRepositoryTests {
+
+    @Autowired
+    private GoalRepository goalRepository;
+
+    // 미션 목록 추가 테스트
+    @Test
+    public void testInsert() {
+
+        String startDateString = "2024-05-09";
+        String endDateString = "2024-06-08";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate startDate = LocalDate.parse(startDateString, formatter);
+        LocalDate endDate = LocalDate.parse(endDateString, formatter);
+
+        IntStream.rangeClosed(1, 50).forEach(i -> {
+            Goal goal = Goal.builder()
+                    .title("goal title..." + i)
+                    .content("goal content..." + i)
+                    .maxPeople(i)
+                    .authCount(i+9)
+                    .complete("모집 중")
+                    .startDate(startDate)
+                    .endDate(endDate)
+                    .build();
+
+            Goal result = goalRepository.save(goal);
+            log.info("goalId: " + result.getGoalId());
+        });
+    }
+
+}
