5f598510be28af883279712fadca97857b254324
서혜리 <hye_dam_2@naver.com>
2024-05-09 17:23:54 +0900

    GoalService 수정

--- src/main/java/com/allrounders/goalkeeper/service/GoalService.java
+++ src/main/java/com/allrounders/goalkeeper/service/GoalService.java
@@ -2,9 +2,10 @@ package com.allrounders.goalkeeper.service;
 
 import com.allrounders.goalkeeper.domain.Goal;
 import com.allrounders.goalkeeper.dto.GoalAddDTO;
-import com.allrounders.goalkeeper.repository.GoalImgRepository;
 import com.allrounders.goalkeeper.repository.GoalRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,14 @@ import org.springframework.stereotype.Service;
 public class GoalService {
 
     private final GoalRepository goalRepository;
-    private final GoalImgRepository goalImgRepository;
 
-    public Integer goalAdd(GoalAddDTO goalAddDTO) {
+    public void goalAdd(GoalAddDTO goalAddDTO) {
         Goal goal = GoalAddDTO.dtoToEntity(goalAddDTO);
-        Integer goalId = goalRepository.save(goal).getGoalId();
-        return goalId;
+        goalRepository.save(goal);
+    }
+
+    public Page<Goal> goalList(Pageable pageable) {
+        Page<Goal> goalPage = goalRepository.findAllOrderByGoalIdDesc(pageable);
+        return goalPage;
     }
 }
