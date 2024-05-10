fa40cdfd8822ba30642d3457e0ff99fa01649815
서혜리 <hye_dam_2@naver.com>
2024-05-10 15:37:59 +0900

    findByGoalId 추가

--- src/main/java/com/allrounders/goalkeeper/repository/GoalRepository.java
+++ src/main/java/com/allrounders/goalkeeper/repository/GoalRepository.java
@@ -4,5 +4,5 @@ import com.allrounders.goalkeeper.domain.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GoalRepository extends JpaRepository<Goal, Integer>, GoalCustomRepository {
-
+    Goal findByGoalId(Long goalId);
 }
