4a7063d28453f02d3ae9788d9e9318d4137ed97e
서혜리 <hye_dam_2@naver.com>
2024-05-10 15:38:40 +0900

    GoalService에 미션 등록 시 memberGoal 추가하는 코드 추가

--- src/main/java/com/allrounders/goalkeeper/service/GoalService.java
+++ src/main/java/com/allrounders/goalkeeper/service/GoalService.java
@@ -1,22 +1,35 @@
 package com.allrounders.goalkeeper.service;
 
 import com.allrounders.goalkeeper.domain.Goal;
+import com.allrounders.goalkeeper.domain.MemberGoal;
 import com.allrounders.goalkeeper.dto.GoalAddDTO;
 import com.allrounders.goalkeeper.repository.GoalRepository;
+import com.allrounders.goalkeeper.repository.MemberGoalRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class GoalService {
 
     private final GoalRepository goalRepository;
+    private final MemberGoalRepository memberGoalRepository;
 
-    public void goalAdd(GoalAddDTO goalAddDTO) {
+    public void goalAdd(GoalAddDTO goalAddDTO, HttpSession session) {
+        Long memberId = (Long)session.getAttribute("memberId");
         Goal goal = GoalAddDTO.dtoToEntity(goalAddDTO);
-        goalRepository.save(goal);
+        Long goalId = goalRepository.save(goal).getGoalId();
+
+        LocalDate startDate = goalRepository.findByGoalId(goalId).getStartDate();
+        LocalDate endDate = goalRepository.findByGoalId(goalId).getEndDate();
+
+        MemberGoal memberGoal = new MemberGoal(memberId, goalId, true, startDate, endDate, false);
+        memberGoalRepository.save(memberGoal);
     }
 
     public Page<Goal> goalList(Pageable pageable) {
