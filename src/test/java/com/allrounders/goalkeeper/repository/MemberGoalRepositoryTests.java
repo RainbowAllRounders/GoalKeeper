7bc5048146e8b652ec0e7f4a4122dd2546a2fd37
서혜리 <hye_dam_2@naver.com>
2024-05-10 15:38:54 +0900

    테스트 코드 추가

--- src/test/java/com/allrounders/goalkeeper/repository/MemberGoalRepositoryTests.java
+++ src/test/java/com/allrounders/goalkeeper/repository/MemberGoalRepositoryTests.java
@@ -1,7 +1,7 @@
 package com.allrounders.goalkeeper.repository;
 
 import com.allrounders.goalkeeper.domain.MemberGoal;
-import groovy.util.logging.Log4j2;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,16 +19,39 @@ public class MemberGoalRepositoryTests {
     @Autowired
     private MemberGoalRepository memberGoalRepository;
 
+    // 미션 생성자
     @Test
     public void testInsert() {
 
         MemberGoal memberGoal = MemberGoal.builder()
                 .member(memberRepository.findByMemberId(1L))
+                .goal(goalRepository.findByGoalId(1L))
                 .role(true)
                 .startAlarmDate(goalRepository.findByGoalId(1L).getStartDate())
                 .endAlarmDate(goalRepository.findByGoalId(1L).getEndDate())
                 .isChecked(false)
                 .isSuccess(false)
                 .build();
+
+        MemberGoal result = memberGoalRepository.save(memberGoal);
+        log.info("memberGoalId: " + result.getMemberGoalId());
+    }
+
+    // 미션 참여자
+    @Test
+    public void testInsert2() {
+
+        MemberGoal memberGoal = MemberGoal.builder()
+                .member(memberRepository.findByMemberId(2L))
+                .goal(goalRepository.findByGoalId(1L))
+                .role(false)
+                .startAlarmDate(goalRepository.findByGoalId(1L).getStartDate())
+                .endAlarmDate(goalRepository.findByGoalId(1L).getEndDate())
+                .isChecked(false)
+                .isSuccess(false)
+                .build();
+
+        MemberGoal result = memberGoalRepository.save(memberGoal);
+        log.info("memberGoalId: " + result.getMemberGoalId());
     }
 }
