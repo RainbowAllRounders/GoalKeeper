e85f7479ba53c96df2165c5275847ae25de7546a
서혜리 <hye_dam_2@naver.com>
2024-05-10 09:35:11 +0900

    hashtagDTO 수정

--- src/main/java/com/allrounders/goalkeeper/dto/HashtagDTO.java
+++ src/main/java/com/allrounders/goalkeeper/dto/HashtagDTO.java
@@ -1,8 +1,6 @@
 package com.allrounders.goalkeeper.dto;
 
-import com.allrounders.goalkeeper.domain.Hashtag;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,22 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HashtagDTO {
-    private Long tagId;
-
-    @NotNull
-    private GoalAddDTO goalAddDTO;
 
     @NotBlank
     private String tagName;
-
-    public static Hashtag dtoToEntity(HashtagDTO hashtagDTO) {
-
-        Hashtag hashtag = Hashtag.builder()
-                .tagId(hashtagDTO.getTagId())
-                .tagName(hashtagDTO.getTagName())
-                .goal(GoalAddDTO.dtoToEntity(hashtagDTO.getGoalAddDTO()))
-                .build();
-
-        return hashtag;
-    }
 }
