8baaa45bfdbfb3cb1a41ed05fa91e6e726a9cd5d
서혜리 <hye_dam_2@naver.com>
2024-05-11 20:54:45 +0900

    GoalCustomRepository 수정

--- src/main/java/com/allrounders/goalkeeper/repository/GoalCustomRepository.java
+++ src/main/java/com/allrounders/goalkeeper/repository/GoalCustomRepository.java
@@ -1,6 +1,7 @@
 package com.allrounders.goalkeeper.repository;
 
 import com.allrounders.goalkeeper.domain.Goal;
+import com.allrounders.goalkeeper.dto.GoalListDTO;
 import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +10,7 @@ public interface GoalCustomRepository {
 
     void applyPagination(Pageable pageable, JPAQuery<Goal> query);
 
-    Page<Goal> findAllOrderByGoalIdDesc(Pageable pageable);
+    Page<GoalListDTO> findAllOrderByGoalIdDesc(Pageable pageable);
 
 //    Page<Goal> searchAll(String[] types, String keyword, Pageable pageable);
 }
--- src/main/java/com/allrounders/goalkeeper/repository/impl/GoalCustomRepositoryImpl.java
+++ src/main/java/com/allrounders/goalkeeper/repository/impl/GoalCustomRepositoryImpl.java
@@ -1,8 +1,11 @@
 package com.allrounders.goalkeeper.repository.impl;
 
 import com.allrounders.goalkeeper.domain.Goal;
+import com.allrounders.goalkeeper.dto.GoalListDTO;
+import com.allrounders.goalkeeper.dto.HashtagDTO;
 import com.allrounders.goalkeeper.repository.GoalCustomRepository;
-import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.AllArgsConstructor;
@@ -12,10 +15,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.allrounders.goalkeeper.domain.QGoal.goal;
-import static com.allrounders.goalkeeper.domain.QHashtag.hashtag;
-import static com.allrounders.goalkeeper.domain.QMemberGoal.memberGoal;
 
 @Repository
 @AllArgsConstructor
@@ -29,22 +31,42 @@ public class GoalCustomRepositoryImpl implements GoalCustomRepository {
     }
 
     @Override
-    public Page<Goal> findAllOrderByGoalIdDesc(Pageable pageable) {
-
-        JPAQuery<Goal> query = jpaQueryFactory.selectFrom(goal)
+    public Page<GoalListDTO> findAllOrderByGoalIdDesc(Pageable pageable) {
+        // 페이지네이션 적용한 쿼리
+        QueryResults<Tuple> queryResults = jpaQueryFactory
+                .select(goal.goalId, goal.title, goal.maxPeople,
+                        goal.curPeople, goal.likeCount, goal.authCount,
+                        goal.complete, goal.startDate, goal.endDate,
+                        goal.hashtagList, goal.imgPath)
+                .from(goal)
                 .orderBy(goal.goalId.desc())
                 .offset(pageable.getOffset())
-                .limit(pageable.getPageSize());
+                .limit(pageable.getPageSize())
+                .fetchResults();
 
-        // query 실행한 결과값
-        List<Goal> content = query.fetch();
+        List<Tuple> tuples = queryResults.getResults();
+        List<GoalListDTO> goalListDTOs = tuples.stream()
+                .map(tuple -> {
+                    GoalListDTO dto = new GoalListDTO();
+                    dto.setGoalId(tuple.get(goal.goalId));
+                    dto.setTitle(tuple.get(goal.title));
+                    dto.setMaxPeople(tuple.get(goal.maxPeople));
+                    dto.setCurPeople(tuple.get(goal.curPeople));
+                    dto.setLikeCount(tuple.get(goal.likeCount));
+                    dto.setAuthCount(tuple.get(goal.authCount));
+                    dto.setComplete(tuple.get(goal.complete));
+                    dto.setStartDate(tuple.get(goal.startDate));
+                    dto.setEndDate(tuple.get(goal.endDate));
+                    dto.setHashtagDTOList(HashtagDTO.fromEntities(tuple.get(goal.hashtagList)));
+                    dto.setImgPath(tuple.get(goal.imgPath));
+                    return dto;
+                })
+                .collect(Collectors.toList());
 
-        // 전체 데이터 개수 파악
-        long total = query.fetchCount();
-
-        return new PageImpl<>(content, pageable, total);
+        return new PageImpl<>(goalListDTOs, pageable, queryResults.getTotal());
     }
 
+    // 검색
 //    @Override
 //    public Page<Goal> searchAll(String[] types, String keyword, Pageable pageable) {
 //
