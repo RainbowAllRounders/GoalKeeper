7144f2e4d80dfcc3c662344807fac3c882c76e91
서혜리 <hye_dam_2@naver.com>
2024-05-13 03:45:01 +0900

    GoalAddDTO 수정 - hashtag 관련 메소드 수정 및 complete 필드 추가

--- src/main/java/com/allrounders/goalkeeper/dto/GoalAddDTO.java
+++ src/main/java/com/allrounders/goalkeeper/dto/GoalAddDTO.java
@@ -4,12 +4,10 @@ import com.allrounders.goalkeeper.domain.Goal;
 import com.allrounders.goalkeeper.domain.Hashtag;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,45 +31,50 @@ public class GoalAddDTO {
     private Integer authCount;
 
     @NotNull
+    private String complete;
+
+    @NotNull
     private LocalDate startDate;
 
     @NotNull
     private LocalDate endDate;
 
-
-    private String hashtagDTOs;
+    private String hashtagDTOs; // 프론트에서 받아오는 해시태그들의 tagName이 연결된 문자열
   
-    private List<HashtagDTO> hashtagDTOList;
+    private List<HashtagDTO> hashtagDTOList = new ArrayList<>();    // hashtagDTOs를 HashtagDTO 형태로 변환
 
 //    @NotBlank
-    private String imgPath;
+//    private String imgPath;
 
     public Goal dtoToEntity() {
-
         changeList(hashtagDTOs);
-        List<Hashtag> hashtagList = this.hashtagDTOList.stream().map(HashtagDTO::dtoToEntity).collect(Collectors.toList());
+        List<Hashtag> hashtagList = this.hashtagDTOList.stream()
+                .map(HashtagDTO::dtoToEntity)
+                .collect(Collectors.toList());
 
-        Goal goal = Goal.builder()
+        return Goal.builder()
                 .title(this.getTitle())
                 .content(this.getContent())
                 .maxPeople(this.getMaxPeople())
                 .authCount(this.getAuthCount())
                 .hashtagList(hashtagList)
+                .complete("모집 중")
                 .startDate(this.getStartDate())
                 .endDate(this.getEndDate())
                 .build();
-
-        return goal;
     }
-
 
     public void changeList(String hashtagDTOs) {
-        HashtagDTO hashtagDTO = new HashtagDTO();
-        String[] split = hashtagDTOs.split("\\s*#\\s*");
+        if (hashtagDTOs != null) {
 
-        for(String tagName : split) {
-            HashtagDTO hash = hashtagDTO.builder().tagName(tagName).build();
-            hashtagDTOList.add(hash);
+            String[] split = hashtagDTOs.split("\\s*#\\s*");
+
+            for (String tagName : split) {
+                HashtagDTO hash = HashtagDTO.builder().tagName(tagName).build();
+                hashtagDTOList.add(hash);
+            }
         }
     }
+
+
 }
