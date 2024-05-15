9f2c7d5ff32a553c4b523bb183e2957007c863ca
Gaannini <0107162a@gmail.com>
2024-05-15 13:47:30 +0900

    Update GameController.java
    + OXGame

--- src/main/java/com/allrounders/goalkeeper/controller/GameController.java
+++ src/main/java/com/allrounders/goalkeeper/controller/GameController.java
@@ -1,7 +1,9 @@
 package com.allrounders.goalkeeper.controller;
 
+import com.allrounders.goalkeeper.service.OXGameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class GameController {
 
+    private final OXGameService oxGameService;
+
     @GetMapping("/game_main")
     public String gameMain() {
         return "/game/game_main";
@@ -17,5 +21,11 @@ public class GameController {
     @GetMapping("/game/bat_game")
     public String batGame() {
         return "/game/bat_game";
+    }
+
+    @GetMapping("/OXGame")
+    public String OXGame(Model model) {
+        model.addAttribute("Questions", oxGameService.fimdRandom5Q());
+        return "game/OXgame";
     }
 }
