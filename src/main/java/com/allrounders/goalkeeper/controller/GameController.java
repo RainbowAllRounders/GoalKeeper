3e59514f48e28ca8ac5dc7d1ff8a2e8ce97e45be
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-05-15 21:01:34 +0900

    feat/bat_game_point

--- src/main/java/com/allrounders/goalkeeper/controller/GameController.java
+++ src/main/java/com/allrounders/goalkeeper/controller/GameController.java
@@ -3,6 +3,7 @@ package com.allrounders.goalkeeper.controller;
 import com.allrounders.goalkeeper.dto.UpdatePointDTO;
 import com.allrounders.goalkeeper.service.GameService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Log4j2
 @Controller
 @RequestMapping("/game")
 @RequiredArgsConstructor
 public class GameController {
 
-    GameService gameService;
+    private final GameService gameService;
 
     @GetMapping("/game_main")
     public String gameMain() {
@@ -32,9 +34,11 @@ public class GameController {
     public ResponseEntity<String> updatePoint(UpdatePointDTO updatePointDTO) {
         try {
             gameService.updateMemberPoints(updatePointDTO);
+            log.info(updatePointDTO);
             return ResponseEntity.ok("포인트 업데이트가 완료됨");
         } catch (Exception e) {
             e.printStackTrace();
+            log.info(updatePointDTO);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("포인트 업데이트가 실패함.");
         }
     }
