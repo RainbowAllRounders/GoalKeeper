9fdbe91163b76558e0c0b84448d70cf21d52125c
서혜리 <hye_dam_2@naver.com>
2024-05-15 22:14:19 +0900

    goalList.css, myGoalList.css 수정

--- src/main/resources/static/js/member/myGoalList.js
+++ src/main/resources/static/js/member/myGoalList.js
@@ -7,24 +7,213 @@ const searchBox = document.querySelector('#searchBox');
 const searchContent = searchBox.querySelector('input');
 const magnifier = document.querySelector('#magnifier');
 
+// goal 목록
+const goalList = document.querySelectorAll(".goalCard");
+
+// 페이지네이션
+const pagination = document.querySelector(".pagination");
+
+// 좋아요 누르기
+let rightContainer = document.querySelectorAll(".rightContainer");
+let hearts = document.querySelectorAll(".heart");
+let likeCount = document.querySelectorAll('.likeCount');
+
 window.addEventListener('load', function() {
+
+    // 모집 중/진행 중/완료 태그 색 변경
+    goalList.forEach(function(goal) {
+        let statusTag = goal.querySelector(".statusTag");
+        let status = statusTag.textContent.trim();
+        const isSuccessString = goal.querySelector('.isSuccess').value;
+        const isSuccess = isSuccessString === 'true';
+        let progressWrap = goal.querySelector('.progressWrap');
+        let progressBarValue = goal.querySelector('.progressBarValue');
+
+        if (status === "모집 중") {
+            statusTag.classList.add("recruiting");
+            progressWrap.classList.add("recruit");
+            progressBarValue.innerHTML = "모 집 중";
+        } else if (status === "진행 중") {
+            statusTag.classList.add("proceeding");
+            progressWrap.classList.add("proceed");
+            progressBarValue.classList.add("proceedValue");
+            progressBarValue.innerHTML = "진 행 중";
+        } else if (status === "완료") {
+            statusTag.classList.add("complete");
+            progressWrap.classList.add("end");
+            progressBarValue.classList.add("end");
+            progressBarValue.innerHTML = "100%";
+
+            // goal.complete가 'complete'이고 goal.isSuccess가 false이면 fail 클래스 추가
+            if (status === "완료" && isSuccess != true) {
+                let cardHeader = goal.querySelector('.cardHeader');
+                let container = goal.querySelector('.rightContainer');
+                let imgWrapper = goal.querySelector('.imgWrapper');
+                let cardFooter = goal.querySelector('.cardFooter');
+                const failImg = new Image();
+                failImg.src = '../../../images/fail.png';
+                failImg.alt = "실패";
+                failImg.classList.add('failImg');
+                failImg.style.width = "275px";
+                failImg.style.height = "180px";
+                failImg.style.opacity = "1";
+
+                cardHeader.classList.add('fail');
+                container.classList.add('fail');
+                imgWrapper.classList.add('failImgWrapper');
+                imgWrapper.classList.remove('imgWrapper');
+                imgWrapper.appendChild(failImg);
+                cardFooter.classList.add('fail');
+            }
+        }
+    });
+
+    // 페이지 변경 시 효과 적용
+    pagination.addEventListener('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const target = e.target;
+
+        const num = target.getAttribute("data-num");
+
+        // 가져온 페이지 번호가 null 또는 undefined인 경우, 즉 data-num 속성이 없는 요소가 클릭된 경우
+        if (num === null || num === undefined) {
+            return; // 함수 종료
+        }
+
+        // 현재 URL 가져오기
+        let currentUrl = window.location.href;
+
+        // URL에 이미 페이지 번호가 포함되어 있는지 확인
+        if (currentUrl.includes('?page=')) {
+            // 페이지 번호가 이미 포함되어 있는 경우, 해당 부분을 새로 클릭된 페이지 번호로 교체
+            currentUrl = currentUrl.replace(/page=\d+/g, 'page=' + num);
+        } else {
+            if (currentUrl.includes('?')) {
+                if (currentUrl.includes('&page=')) {
+                    // URL에 page가 포함되어 있는 경우
+                    currentUrl = currentUrl.replace(/page=\d+/g, 'page=' + num);
+                } else {
+                    // URL에 이미 다른 파라미터가 있는 경우
+                    currentUrl += '&page=' + num;
+                }
+            } else {
+                // URL에 파라미터가 없는 경우
+                currentUrl += '?page=' + num;
+            }
+        }
+
+        // 현재 활성화된 페이지 번호에 active 클래스를 제거
+        pagination.querySelectorAll('.page-item').forEach(function(item) {
+            item.classList.remove('active');
+        });
+
+        // 클릭한 페이지 번호에 active 클래스를 추가
+        target.closest('.page-item').classList.add('active');
+
+        // 새로운 URL로 이동
+        window.location.href = currentUrl;
+    });
 
     // 필터 선택 시 효과 --------------------
     filters.forEach(function(filter) {
+        // 현재 URL에 해당 필터가 적용되어 있는지 확인
+        const currentUrl = window.location.href;
+        const filterType = filter.id;
+
+        if (currentUrl.includes('type=' + filterType)) {
+            // URL에 해당 필터가 적용되어 있는 경우
+            filter.classList.add('clickedFilter');
+        }
+
         filter.addEventListener('click', function() {
-
-            // 모든 필터 색 초기화
+            // 클릭된 필터에 clickedFilter 클래스 추가
             filters.forEach(function(f) {
-                f.style.border = '1px solid #B0B0B0';
-                f.style.backgroundColor = 'transparent';
-                f.style.color = '#B0B0B0';
+                f.classList.remove('clickedFilter');
             });
+            filter.classList.add('clickedFilter');
 
-            // 선택된 필터에 대한 스타일 변경
-            filter.style.border = 'none';
-            filter.style.backgroundColor = '#FFC933';
-            filter.style.color = 'black';
+            // 클릭된 필터에 해당하는 페이지로 이동
+            const type = filter.id;
+            const formObj = document.querySelector('#filterForm');
+            formObj.action = '/myPage/myGoalList';
+            formObj.querySelector('#textInput').value = type;
+            formObj.submit();
         })
+    });
+
+    document.querySelector('#r').addEventListener('click', (e)=>{
+        e.stopPropagation();
+        e.preventDefault();
+
+        const formObj = document.querySelector('#filterForm');
+
+        formObj.action = '/myPage/myGoalList';
+        formObj.querySelector('#textInput').value = 'r';
+        formObj.submit();
+    });
+
+    document.querySelector('#p').addEventListener('click', (e)=>{
+        e.stopPropagation();
+        e.preventDefault();
+
+        const formObj = document.querySelector('#filterForm');
+
+        formObj.action = '/myPage/myGoalList';
+        formObj.querySelector('#textInput').value = 'p';
+        formObj.submit();
+    });
+
+    document.querySelector('#c').addEventListener('click', (e)=>{
+        e.stopPropagation();
+        e.preventDefault();
+
+        const formObj = document.querySelector('#filterForm');
+
+        formObj.action = '/myPage/myGoalList';
+        formObj.querySelector('#textInput').value = 'c';
+        formObj.submit();
+    });
+
+    rightContainer.forEach(function (container, index) {
+        container.addEventListener('click', function () {
+            // 현재 클릭된 카드의 좋아요 상태
+            const isLiked = hearts[index].getAttribute('src') === '/images/redHeart.png';
+
+            // 현재 클릭된 카드의 좋아요 개수
+            let currentLikeCount = parseInt(likeCount[index].textContent);
+
+            // 좋아요 상태를 서버에 전송하여 변경
+            fetch('/goal/like', {
+                method: 'GET',
+                credentials: 'same-origin' // 세션 쿠키를 함께 보냄
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        // 좋아요 상태 변경 및 좋아요 개수 업데이트
+                        if (isLiked) {
+                            // 이미 좋아요를 누른 상태이면 grayHeart로 변경하고 좋아요 수 감소
+                            hearts[index].setAttribute('src', '/images/grayHeart.png');
+                            currentLikeCount--;
+                        } else {
+                            // 좋아요를 누르지 않은 상태이면 redHeart로 변경하고 좋아요 수 증가
+                            hearts[index].setAttribute('src', '/images/redHeart.png');
+                            currentLikeCount++;
+                        }
+
+                        // 좋아요 개수 업데이트
+                        likeCount[index].textContent = currentLikeCount;
+                    } else {
+                        // 좋아요 상태 변경에 실패한 경우
+                        console.error('Failed to update like status');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error occurred while updating like status:', error);
+                });
+        });
     });
 
     // 검색 --------------------
