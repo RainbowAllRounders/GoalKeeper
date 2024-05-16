c102dbdfbde0f0d671b9d93133a1d5a35c5e9031
서혜리 <hye_dam_2@naver.com>
2024-05-16 13:01:49 +0900

    UploadController 돌려놓기

--- src/main/java/com/allrounders/goalkeeper/controller/UploadController.java
+++ src/main/java/com/allrounders/goalkeeper/controller/UploadController.java
@@ -2,9 +2,9 @@ package com.allrounders.goalkeeper.controller;
 
 import com.allrounders.goalkeeper.dto.UploadFileDTO;
 import com.allrounders.goalkeeper.dto.UploadResultDTO;
-import com.allrounders.goalkeeper.service.ImgService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -17,8 +17,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,15 +33,47 @@ public class UploadController {
     @Value("${com.allrounders.upload.path}")
     private String uploadPath;
 
-    private final ImgService imgService;
-
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO){
 
         log.info(uploadFileDTO);
 
         if(uploadFileDTO.getFiles() != null){
-            imgService.uploadImg(uploadFileDTO, uploadPath);
+            final List<UploadResultDTO> list = new ArrayList<>();
+
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+
+                String originalName = multipartFile.getOriginalFilename();
+
+                String uuid = UUID.randomUUID().toString();
+
+                Path savePath = Paths.get(uploadPath, uuid+"_"+ originalName);
+
+                boolean image = false;
+
+                try {
+                    multipartFile.transferTo(savePath);
+
+                    if(Files.probeContentType(savePath).startsWith("image")){
+
+                        image = true;
+
+                        File thumbFile = new File(uploadPath, "s_" + uuid+"_"+ originalName);
+
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200,200);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                list.add(UploadResultDTO.builder()
+                        .uuid(uuid)
+                        .fileName(originalName)
+                        .img(image)
+                        .build()
+                );
+            });
+            return list;
         }
         return null;
     }
